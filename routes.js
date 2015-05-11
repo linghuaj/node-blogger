d320de1e92fb2a63dbd4861dd87b50dc72eb1fac
Linghua Jin <ljin@walmartlabs.com>
2015-05-11 15:54:14 -0700

    added post

--- routes.js
+++ routes.js
@@ -1,5 +1,10 @@
+let fs = require('fs')
+let multiparty = require('multiparty')
+let then = require('express-then')
 let isLoggedIn = require('./middleware/isLoggedIn')
-
+let Post = require('./models/post')
+// let util = require('util')
+let DataUri = require('datauri')
 module.exports = (app) => {
     let passport = app.passport
 
@@ -46,16 +51,63 @@ module.exports = (app) => {
 
 
 
-    app.get('/post/:postId?', (req, res) => {
-        let postId = req.params.postId
-        if (!postId){
-          res.render('post.ejs', {
-            post: {},
-            verb: 'Create'
+  app.get('/post/:postId?', then(async(req, res) => {
+      let postId = req.params.postId
+      if (!postId) {
+        res.render('post.ejs', {
+          post: {},
+          verb: 'Create'
         })
+        return
+      }
+      let post = await Post.promise.findById(postId)
+      if (!post) res.status(404).send('Not found')
 
-        }
+      let dataUri = new DataUri()
+      let image
+      let imageData
+      if (post.image.data) {
+        image = dataUri.format('.' + post.image.contentType.split('/').pop(), post.image.data)
+        imageData = `data:${post.image.contentType};base64,${image.base64}`
+      }
+      console.log(">< image", image)
+      res.render('post.ejs', {
+        post: post,
+        verb: 'Edit',
+        image: imageData
+      })
 
-    })
+    }))
+
+  app.post('/post/:postId?', then(async(req, res) => {
+      let postId = req.params.postId
+      let post
+      if (!postId) {
+          post = new Post()
+      } else {
+          post = await Post.promise.findById(postId)
+          if (!post) res.status(404).send('Not found')
+      }
+
+      let [{title: [title], content: [content]}, {image: [file]}] = await new multiparty.Form().promise.parse(req)
+      post.title = title
+      post.content = content
+      console.log(file, title, content)
+      if (file.originalFilename !== '') {
+        post.image.data = await fs.promise.readFile(file.path)
+        post.image.contentType = file.headers['content-type']
+      }
+      //console.log(file, title, content, post)
+
+      // if (!postId) {
+      //     // assign user id to the post
+      //     post.user_id = req.user._id
+      // }
+      await post.save()
+      //TODO
+      res.redirect('/blog/' + encodeURI(req.user.blogTitle))
+      return
+   }))
+
 
 }
